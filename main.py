1b29b8f1cc51a44156cd229e7fafd6010173624b
Gustavo <gustavomacedosousa013@gmail.com>
2023-08-06 20:25:29 -0300

    Recebendo o input para primeiro teste

--- main.py
+++ main.py
@@ -3,4 +3,17 @@ ex_tabuleiro = [['X O X'], ['O X O'], ['X O X']]
 for x in range(len(ex_tabuleiro)):
     print(ex_tabuleiro[x])
 
+print("Preencha os campos _________ com simbolos X, O ou _ para deixar em branco")
+linha = input("Digite: ").upper()
+if len(linha) < 9 or len(linha) > 9:
+    print('Tamanho inválido')
+else:
+    print(f"""
+    ---------
+    | {linha[0]} {linha[1]} {linha[2]} |
+    | {linha[3]} {linha[4]} {linha[5]} |
+    | {linha[6]} {linha[7]} {linha[8]} |
+    ---------
+    """)
+
 
